8f4a524c36979de9f4c6a9d7e8883ebbf8a42553
Dhananjay <dhananjaysarsonia@outlook.com>
2020-10-30 20:31:56 -0400

    Initial commit

---
+++ PastryAlgorithm/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Runtime.CompilerServices
+open System.Threading.Tasks
+open Akka
+open Akka.Actor
+open Akka.Dispatch.SysMsg
+open Akka.FSharp
+open Akka.Actor
+open System.Diagnostics
+open Akka.Util
+open System.Threading;
+
+
+let nNodes = 100
+let nRequest = 5
+
+let digits = int <| ceil(Math.Log(float <| nNodes) / Math.Log(float <| 16))
+
+
+
+
+
+
+
+
+
+
+
