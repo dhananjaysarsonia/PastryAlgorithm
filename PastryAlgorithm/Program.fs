a71b5eb973ba75620fa291584d7a23bdfa3e07f3
Dhananjay <dhananjaysarsonia@outlook.com>
2020-10-31 03:04:27 -0400

    peer actor in progress. writing initialize

--- PastryAlgorithm/Program.fs
+++ PastryAlgorithm/Program.fs
@@ -17,6 +17,56 @@ let nNodes = 100
 let nRequest = 5
 
 let digits = int <| ceil(Math.Log(float <| nNodes) / Math.Log(float <| 16))
+let col = 16
+type Message =
+    | Initialize of String
+    | Joining
+    | UpdateRouting
+    | UpdateLeaves
+    
+    
+let hexToDec (hex: String) =
+    Convert.ToInt32(hex, 16)
+
+let decToHex(dec : int) =
+    dec.ToString("X")
+    
+let peer(mailbox : Actor<_>) =
+    let mutable peerId = ""
+    let mutable routingTable : string [,] = Array2D.zeroCreate 0 0
+    let smallLeafSet = Set.empty
+    let bigLeafSet = Set.empty
+    let rec loop() = actor{
+        let! message = mailbox.Receive ()
+        match message with
+        |Initialize(id) ->
+            peerId <- id
+            routingTable <- Array2D.zeroCreate digits col
+            //let decId = hexToDec <| peerId
+            
+            
+            
+        
+        
+        
+        return! loop()
+    }
+    loop()
+    
+    
+
+
+
+
+let nodeId: String = String.replicate digits "0"
+printf "%A" nodeId
+
+
+
+
+
+
+
 
 
 
